94711621d9c5ffe350d6bff50dccf9800f145234
webarserker <195073267@qq.com>
2017-12-27 00:04:56 +0800

    error

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader' //eslint-disable-line
+import { AppContainer } from 'react-hot-loader'
 import App from './App.jsx'
 // ReactDOM.hydrate(<App />, document.getElementById('root'))
 
